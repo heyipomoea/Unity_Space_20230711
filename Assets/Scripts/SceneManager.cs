3e83120fa4865a8a972abbc14b5fa851bd0ffaca
heyipomoea <heyipomoea@gmail.com>
2023-08-08 10:17:55 +0800

    Update 切換場景

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 
@@ -8,6 +9,18 @@ namespace heyipomoea
     /// </summary>
     public class SceneManager : MonoBehaviour
     {
+        public void ChangScene()
+        {
+            print("切換遊戲");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("遊戲場景");
+        }
 
+       
+
+        public void Quit()
+        {
+            print("離開遊戲");
+            Application.Quit();
+        }
     }
 }
