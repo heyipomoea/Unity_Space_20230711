ca2846d827c7791736b31287dc95b8df7065fb5e
heyipomoea <heyipomoea@gmail.com>
2023-09-19 16:23:28 +0800

    Update 敵人受傷系統

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+
+namespace Heyipomoea.TwoD
+{
+    /// <summary>
+    /// 子彈
+    /// </summary>
+    public class Bullet : MonoBehaviour
+    {
+        [Header("玩家資料")]
+        public DataBasic dataPlayer;
+
+        private void Awake()
+        {
+            Destroy(gameObject, 5);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
---
+++ Assets/Scripts/DamageEnemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+namespace Heyipomoea.TwoD
+{
+    public class DamageEnemy : DamageSystem
+    {
+        private string nameBullet = "子彈";
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            //print($"碰到的物件: {collision.gameObject}");
+            if(collision.gameObject.name.Contains(nameBullet))
+            {
+                float bulletAttack = collision.gameObject.GetComponent<Bullet>().dataPlayer.attack;
+                Damage(bulletAttack);
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
+
