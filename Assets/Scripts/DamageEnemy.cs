9a390d0f68ef91d94721e02bb716015bef9d66b6
heyipomoea <heyipomoea@gmail.com>
2023-09-21 10:15:44 +0800

    Update 死亡動畫 + 掉落物品

--- Assets/Scripts/DamageEnemy.cs
+++ Assets/Scripts/DamageEnemy.cs
@@ -9,8 +9,22 @@ namespace Heyipomoea.TwoD
         private StateManager stateManager;
         [SerializeField, Header("受傷狀態")]
         private StateHit stateHit;
+        [SerializeField, Header("掉落道具")]
+        private GameObject prefabItem;
 
         private string nameBullet = "子彈";
+
+        private Animator ani;
+        private string parDead = "開關死亡";
+        private Rigidbody2D rig;
+        private Collider2D col;
+
+        private void Start()
+        {
+            ani = GetComponent<Animator>();
+            rig = GetComponent<Rigidbody2D>();
+            col = GetComponent<Collider2D>();
+        }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
@@ -25,7 +39,13 @@ namespace Heyipomoea.TwoD
         }
         protected override void Dead()
         {
-            Destroy(gameObject);
+            ani.SetBool(parDead, true);
+            stateManager.enabled = false;
+            rig.bodyType = RigidbodyType2D.Kinematic;
+            col.enabled = false;
+            rig.velocity = Vector3.zero;
+            GameObject tempItem = Instantiate(prefabItem, transform.position + Vector3.up, Quaternion.identity);
+            tempItem.GetComponent<Rigidbody2D>().AddForce(new Vector2(10, 100));
         }
     }
 }
